618ffa7f1c5aae29a0185db1f41eef71bad0a329
heywinit <heywinit@gmail.com>
2025-04-07 20:39:42 +0530

    new proj

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import HomePage from "./pages/HomePage.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+]);
+
+const root = document.getElementById("root");
+
+if (!root) {
+	throw new Error("Root element not found");
+}
+
+createRoot(root).render(<RouterProvider router={router} />);
