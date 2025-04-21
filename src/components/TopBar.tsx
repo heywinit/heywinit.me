446f72074948e5f4d84e80b6f9f74b7ca6baf268
heywinit <heywinit@gmail.com>
2025-04-21 18:27:29 +0530

    new layout

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -13,45 +13,47 @@ export default function TopBar() {
 	};
 
 	return (
-		<div className="flex justify-between items-center w-full px-8 py-4 bg-card backdrop-blur-sm font-mono border border-primary rounded-sm">
-			<div className="flex items-center gap-2">
-				{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
-				<h1
-					className="text-xl font-bold text-primary cursor-pointer select-none"
-					onClick={() => {
-						navigator.clipboard.writeText("hey@winit.dev");
-						setCopied(true);
-						setTimeout(() => {
-							setCopied(false);
-						}, 1000);
-					}}
-				>
-					<motion.span
-						initial={{ opacity: 0 }}
-						animate={{ opacity: 1 }}
-						exit={{ opacity: 0 }}
-						transition={{ duration: 0.3 }}
+		<div className="flex flex-col items-center justify-center w-full bg-primary p-1 pb-0">
+			<div className="flex justify-between items-center w-full px-4 py-4 bg-card backdrop-blur-sm font-mono border border-primary rounded-sm">
+				<div className="flex items-center gap-2">
+					{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+					<h1
+						className="text-xl font-bold text-primary cursor-pointer select-none"
+						onClick={() => {
+							navigator.clipboard.writeText("hey@winit.dev");
+							setCopied(true);
+							setTimeout(() => {
+								setCopied(false);
+							}, 1000);
+						}}
 					>
-						{copied ? "copied!" : "hey@winit.dev"}
-					</motion.span>
-				</h1>
-			</div>
-			<div className="flex items-center gap-4">
-				<nav className="flex items-center gap-4">
-					{["/blogs", "/about", "/contact"].map((path) => (
-						<Link
-							key={path}
-							to={path}
-							className="text-foreground hover:text-primary border border-transparent hover:border-primary/50 px-3 py-1 rounded-sm uppercase tracking-wide text-sm transition-colors"
+						<motion.span
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							transition={{ duration: 0.3 }}
 						>
-							<span className="underline">{path.charAt(1)}</span>
-							{path.slice(2)}
-						</Link>
-					))}
-					<Button variant="solana" size="sm" onClick={toggleTheme}>
-						{theme === "light" ? "D" : "L"}
-					</Button>
-				</nav>
+							{copied ? "copied!" : "hey@winit.dev"}
+						</motion.span>
+					</h1>
+				</div>
+				<div className="flex items-center gap-4">
+					<nav className="flex items-center gap-4">
+						{["/blogs", "/about", "/contact"].map((path) => (
+							<Link
+								key={path}
+								to={path}
+								className="text-foreground hover:text-primary border border-transparent hover:border-primary/50 px-3 py-1 rounded-sm uppercase tracking-wide text-sm transition-colors"
+							>
+								<span className="underline">{path.charAt(1)}</span>
+								{path.slice(2)}
+							</Link>
+						))}
+						<Button variant="solana" size="sm" onClick={toggleTheme}>
+							{theme === "light" ? "D" : "L"}
+						</Button>
+					</nav>
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,10 @@
-import HeroSection from "@/components/sections/HeroSection";
 import TopBar from "@/components/TopBar";
-
+import HeroSection from "@/components/sections/HeroSection";
 export default function HomePage() {
 	return (
-		<div className="bg-black p-4 rounded-lg min-h-screen">
-			<div className="container mx-auto max-w-6xl flex flex-col items-center space-y-4">
-				<TopBar />
-				<HeroSection />
-			</div>
+		<div className="flex flex-col rounded-lg min-h-screen h-screen">
+			<TopBar />
+			<HeroSection />
 		</div>
 	);
 }
